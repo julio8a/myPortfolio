c4d1f50c138d1bd02b7a488a4984eb3b1365ff2e
Julio Ochoa <jochoa@esri.com>
2018-04-09 09:59:28 +0200

    added mustache tempaltes

--- js/template.js
+++ js/template.js
@@ -13,13 +13,22 @@ $(function() {
     var blockResults = Mustache.to_html(blockTemplate, data);
     $('#blocks-container').append(blockResults);
 
+    var iconTemplate = $('#iconTemplate').html();
+    var iconResults = Mustache.to_html(iconTemplate, data);
+    $('#project-menu .center').append(iconResults);
+
+    var aboutTemplate = $('#aboutTemplate').html();
+    var aboutResults = Mustache.to_html(aboutTemplate, data);
+    $('#aboutMe').append(aboutResults);
+
+    var navigationTemplate = $('#navigationTemplate').html();
+    var navigationResults = Mustache.to_html(navigationTemplate, data);
+    $('nav .wrapper ul').append(navigationResults);
+
     var socialTemplate = $('#socialTemplate').html();
     var socialResults = Mustache.to_html(socialTemplate, data);
     $('footer .center').append(socialResults);
 
-    var iconTemplate = $('#iconTemplate').html();
-    var iconResults = Mustache.to_html(iconTemplate, data);
-    $('#project-menu .center').append(iconResults);
   }); //getJSON
   
 }); //function
