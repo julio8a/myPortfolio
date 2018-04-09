ce4304a27a29e2c25baffeeb72e75acf6bbed1e9
Julio Ochoa <jochoa@esri.com>
2018-04-09 12:02:06 +0200

    removed imagemin from default grunt tasks. Will run individually as new image need to be optimized

--- gruntfile.js
+++ gruntfile.js
@@ -96,7 +96,6 @@ module.exports = function(grunt) {
         'default', [
             'concat', 
             'sass', 
-            'imagemin', 
             'connect', 
             'watch'
         ]);
